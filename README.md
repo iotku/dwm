43953ff251eaaf067bb573421b2b5693dfba2819
Jonathan A <me@iotku.pw>
2022-01-23 14:23:31 -0800

    Add rmaster patch

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Patches
 - [actualfullscreen](https://dwm.suckless.org/patches/actualfullscreen/) - Mod+f to properly fullscreen window
 - [attachbottom](https://dwm.suckless.org/patches/attachbottom/) - New clients attach at the bottom of the stack instead of the top. (M Window doesn't move and others resize)
 - [dragmfact](https://dwm.suckless.org/patches/dragmfact/) - Resize the split in the tile layout (e.g. modify mfact) by holding the modkey and dragging the mouse
+- [rmaster](https://dwm.suckless.org/patches/rmaster/) - Press Mod+r to swap side of M window.
 - [statusallmons](https://dwm.suckless.org/patches/statusallmons/) - Show the status bar on all monitors
 - [swallow](https://dwm.suckless.org/patches/swallow/) - Swallow windows opened in terminals (i.e. mpv video playback)
 - [systray](https://dwm.suckless.org/patches/systray/) - A simple system tray implementation.
--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int rmaster            = 0;        /* 1 means master-area is initially on the right */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -89,6 +90,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_r,      togglermaster,  {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
