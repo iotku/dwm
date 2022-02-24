aea8315c9e67f08684e64143d2a8f04815b7981c
Jonathan A <me@iotku.pw>
2022-02-24 12:15:40 -0800

    Use Rofi for launcher and tokyodarkish color scheme

--- config.h
+++ config.h
@@ -9,20 +9,22 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 0;     /* 0 means bottom bar */
+static const int showsystray        = 1;        /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:size=10", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+
+static const char color_fg[] = "#a9b1d6";
+static const char color_fg_selected[] = "#bb9af5";
+static const char color_bg[] = "#1d202f";
+static const char color_bg_selected[] = "#1d202f";
+static const char color_border[] = "#336ae1";
+static const char color_border_selected[] = "#bb9af5";
+
+// fg, bg, border
+static const char *colors[][3] = {
+	[SchemeNorm] = {color_fg, color_bg, color_border},
+	[SchemeSel] = {color_fg_selected, color_bg_selected, color_border_selected},
 };
 
 /* tagging */
@@ -75,8 +77,11 @@ static const Layout layouts[] = {
 #define STATUSBAR "dwmblocks"
 	
 /* commands */
+// Note: Even though we're using rofi now we can't remove these as dwm will fail to compile and I don't feel like patching it out
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { NULL }; 
+
+static const char *roficmd[] = { "rofi", "-combi-modi", "window,drun,ssh", "-theme", "solarized", "-font", "hack 10", "-show", "combi", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /* misc */
@@ -85,7 +90,7 @@ static const char *ssRect[] = { "bash", "-c", "maim -s | xclip -selection clipbo
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
